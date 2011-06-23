4de632c4d11e2c327c45be8f7d12ced6102682ec
Rick Bychowski <wrinkles@gmail.com>
2011-06-23 14:47:12 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,98 @@
+TemplateSelector extends entries and pages with a new "TemplateSelector" custom field. The "TemplateSelector" tag can output the name of any template in your blog.
+
+# USAGE
+Within entry or page context of a template, use the tag to include a template by name:
+
+    <mt:Include name="<mt:TemplateSelector>">
+
+Or perhaps use the tag to set a variable:
+
+    <mt:If tag="TemplateSelector"><mt:TemplateSelector setvar="my_template_name"></mt:If>
+
+Then later in that template set a custom stylesheet:
+
+    <link id="my_custom_layout" rel="stylesheet" href="<mt:Link template="$my_template_name">" type="text/css" media="screen" />
+
+# SETTING THE VALUE
+
+Simply select a template name from a pull-down menu within the "edit entry/page" form.
+
+# TEMPLATE SELECTOR MENU BUILDER
+
+TemplateSelector can automatically search and present your entire list of templates. You probably don't want to present your entire list of templates in the selection menu, so the Preferences::Plugin Settings::TemplateSelector form presents three options to narrow down the list. The resulting list is the result of an intersection of the three options (e.g. list = Type AND Outfile AND Name).
+
+The resulting template list is then used to build the Template Selector Default Form (on plugin options page) and the Template Selector form on each "edit entry/page" form.
+
+## Template Type (optional)
+
+Select from a single template type, or choose blank to return all types (blank is default).
+
+* index - an Index Template
+* archive - an Archive Template
+* category - also an Archive Template
+* individual - also an Archive Template
+* custom - a Template Module.
+* comments -  a Comment Listing Template
+* comment_preview - a Comment Preview Template
+* comment_error - a Comment Error Template
+* popup_image - an Uploaded Image Popup Template
+* BLANK - returns all types (default).
+
+## Index Template Outfile (optional - requires Index type selection)
+When the Index type is active, this text field matches against the Index Template Outfile path/name (precisely as shown in the "Design::Themes" panel of your blog's dashboard). Allows only certain characters (see below). Supports any combination of percent '%' and '_' wildcards. For example:
+
+* '%.css' matches index style sheets.
+* 'archives/%' matches any index files written to your blog root archives directory.
+
+## Template Name (optional)
+A simple text field matching the template name(s). Practically useful only when used with the wildcard characters (otherwise you can only possibly match a single template).
+
+* Entry% - matches 'Entry', 'Entry Monthly', and 'Entry Listing'.
+* Foo_Ba% - matches 'FoodBar', 'FootBall', 'Foo_Bar', etc.
+
+## Allowed Characters for Outfile and Name fields
+* Word characters - alphanumeric plus underscore.
+* Whitespace - allowed (optionally trim trailing/leading whitespace).
+* Symbols (outfile) - Hyphen '-', period '.', and forward slash '/'.
+* Symbols (name) - Hyphen '-' and period '.' .
+* Wildcard - Percent '%' matches any characters, underscore "_" matches a single character.
+
+## Trim Leading/Trailing Whitespace checkbox
+When checked, leading/trailing whitespace will be trimmed from the Outfile and Name fields. Default is checked.
+
+## Blank Selection (Type, Outfile, and Name)
+The default selection is blank. Leaving all three fields blank will (theoretically) return all templates on your system. However, the template list size limit is twenty (see BUGS section below).
+
+# TEMPLATE SELECTOR DEFAULT VALUE
+After saving the three menu builder options, the "Set Default Template" menu will update the list of templates available in the "Set Default Template" field in the plugin options page. So it doesn't make sense to attempt to change the "Menu Builder" and "Default Template" values at the same time (although in fact they are saved in the same form, see Known Bugs section).
+
+# ENTRY/PAGE TEMPLATE SELECTOR
+Template Selector surfaces a dropdown list populated with the templates derived from the plugin options. New entries/pages will be provided the default selection option from the plugin options page.
+
+# INSTALLATION
+To install this plugin follow the instructions found here:
+
+http://tinyurl.com/easy-plugin-install
+
+# DEPENDENCIES
+Melody or Movable Type 4 with __ConfigAssistant 2.1.33 or above__. (ConfigAssistant 2.1.33 [added the options basename to the option_hash for callbacks](https://github.com/openmelody/mt-plugin-configassistant/commit/2e80e4edf7de4fbe6a05df2c11b0f55729d9e974).
+
+# KNOWN BUGS
+My testing has revealed that only the first 20 templates are exposed in the selection form, regardless of how many template names are fed to the Template Selector default value form. I'm open to suggestions to fix that.
+
+Changes to the menu builder and default template options are submitted at the same time (with a single "Save Changes" button. Because the former affects the latter, it makes more sense to submit them separately.
+
+# THANKS
+Thanks to Byrne Reese for guidance, support, and code examples in his PageLayout plugin for Melody.
+
+Thanks also to the Melody team for ConfigAssistant and code examples from the Supr plugin.
+
+# COPYRIGHT AND LICENSE
+
+The software is released under the Artistic License. The
+terms of the Artistic License are described at
+http://www.perl.com/language/misc/Artistic.html .
+
+Except where otherwise noted, TemplateSelector is Copyright 2011,
+Rick Bychowski, rick@hiranyaloka.com for Hiranyaloka. All
+rights reserved.
---
+++ plugins/TemplateSelector/lib/TemplateSelector/Plugin.pm
@@ -0,0 +1,225 @@
+package TemplateSelector::Plugin;
+use strict;
+use MT::Template;
+
+# EDIT ENTRY/PAGE CALLBACK - insert template selection menu for TemplateSelector tag config
+# some code lifted from Endevver's Supr plugin and Byrne Reese's PageLayout plugin
+sub edit_entry_param {
+  my ($cb, $app, $param, $tmpl) = @_;
+  my $blog = $app->blog;
+  my ($entry, $template_selector);
+  # retrieve if previous template selector value
+  if ($param->{id}) {
+    my $type = $param->{object_type};
+    my $class = $app->model($type);
+    $entry = $class->load($param->{id});
+    $template_selector = $entry->template_selector();
+  }
+  #retrieve stored default value
+  my $blog_id = $blog->id;
+  my $plugin = MT->component("TemplateSelector");
+  my $template_selector_default = $plugin->get_config_value( 'template_selector_default',
+    'blog:' . $app->blog->id );
+  $app->log({
+    message => "Default value is '"
+      . $template_selector_default . "'."
+  });
+  # retrieve stored template selector menu
+  my $ts_menu_string;
+  $ts_menu_string = $blog->meta('ts_menu_string');  
+  my @ts_options = split( /\s*\|\s*/, $ts_menu_string );
+  push @ts_options, '';
+  # generate template selector menu
+  my $opts;
+  foreach (@ts_options) {
+    my $selected = ($template_selector_default eq $_);
+    $opts .= '<option value="'.$_.'"'.($selected ? ' selected' : $template_selector_default)
+      .'>'.$_."</option>\n";
+  }
+  my $setting = $tmpl->createElement('app:setting', { 
+    id => 'template_selector', label => "Template Selector" });
+  $setting->innerHTML('<select name="template_selector">'.$opts.'</select>');
+  $tmpl->insertAfter($setting,$tmpl->getElementById('authored_on'));    
+
+  $param;
+}
+
+# EDIT ENTRY CALLBACK - Save selected template name to db
+sub cms_post_save_entry {
+  my ( $cb, $entry, $entry_orig ) = @_;
+  my $ts_selection;
+  my $app = MT->app;
+  return unless $app->isa('MT::App');
+  my $q = $app->can('query') ? $app->query : $app->param;
+  if ( $app->can('param') ) {
+    $ts_selection = $q->param('template_selector');
+  }
+  if ($ts_selection) {
+    $entry->template_selector($ts_selection);
+    $entry->save;
+  }
+  return 1;
+}
+
+# EDIT PAGE CALLBACK - Save selected template name to db
+sub cms_post_save_page {
+  my ( $cb, $page, $page_orig ) = @_;
+  my $ts_selection;
+  my $app = MT->app;
+  return unless $app->isa('MT::App');
+  my $q = $app->can('query') ? $app->query : $app->param;
+  if ( $app->can('param') ) {
+    $ts_selection = $q->param('template_selector');
+  }
+  if ($ts_selection) {
+    $page->template_selector($ts_selection);
+    $page->save;
+  }
+  return 1;
+}
+
+# TAG - returns the template name
+sub _hdlr_template_selector {
+  my ( $ctx, $args ) = @_;
+  my $blog = $ctx->stash('blog');
+  my $blog_id = $blog->id;
+  my $plugin = MT->component("TemplateSelector");
+  my $template_selector_default = $plugin->get_config_value( 'template_selector_default',
+    'blog:' . $blog_id );
+  my $entry = $ctx->stash('entry');
+  return '' if !$entry;
+  return $entry->meta('template_selector') || $template_selector_default || '';
+}
+
+# CUSTOM CONFIG_TYPE - Blog-level template selector menu
+sub _hdlr_ts_menu {
+  my $app = shift;
+  my ($field_id, $field, $value) = @_;
+  my $blog = $app->blog;
+  my $ts_menu_string = $blog->meta('ts_menu_string');
+  
+  my $plugin = MT->component("TemplateSelector");
+  my $template_selector_default = $plugin->get_config_value( 'template_selector_default',
+    'blog:' . $app->blog->id );
+  my @ts_options = split( /\|/, $ts_menu_string );
+  push @ts_options, '';
+  my $class  = 'class="ts_menu"';
+  my $type   = 'type="select"';
+  my $options;
+  foreach my $opt (@ts_options) {
+    my $selected = ($template_selector_default eq $opt);
+    $options .= '<option value="'.$opt.'"'.($selected ? ' selected' : '').'>'.$opt."</option>\n";
+    }
+  return '<select name="template_selector_default">' . $options . '</select>' ;
+}
+
+# OPTIONS CALLBACK - template options
+# also creates concatenated template name list and saves to db
+sub ts_options {
+  my ($cb, $app, $option_hash, $old_value, $new_value) = @_;
+ 
+ unless ($old_value eq $new_value) {
+ 
+ my $blog = $app->blog;
+  my $blog_id = $blog->id;
+  my $scope = "blog:" . $blog_id;
+  my $option = $option_hash->{basename};
+  $app->log({
+    message => "Changing " . $option
+      . " from '" . $old_value 
+      . "' to '" . $new_value . "'."
+  });
+
+  # get the option values
+  my $plugin = MT->component("TemplateSelector");
+  my $ts_trim = ($option eq 'ts_trim') ? $new_value : $plugin->get_config_value( 'ts_trim', $scope );
+  my $ts_type_string = ($option eq 'ts_type') ? $new_value : $plugin->get_config_value( 'ts_type', $scope );
+  my $ts_outfile_string = ($option eq 'ts_outfile') ? $new_value : $plugin->get_config_value( 'ts_outfile', $scope );
+  my $ts_name_string = ($option eq 'ts_name') ? $new_value : $plugin->get_config_value( 'ts_name', $scope );
+
+  # untaint the outfile option 
+  {
+    $ts_outfile_string =~ /^([\w\s\.\-\/%]+)$/;
+    $ts_outfile_string = $1;
+  }
+  # untaint the name option
+  {
+    $ts_name_string =~ /^([\w\s\.\-\/%]+)$/;
+    $ts_name_string = $1;
+  }
+
+  # trim leading/trailing whitespace if ts_trim option is set
+  if ($ts_trim) {
+    $ts_outfile_string = ts_trim_whitespace($ts_outfile_string);
+    $ts_name_string = ts_trim_whitespace($ts_name_string);
+  }
+ 
+   $app->log({
+    message => "ts_type_string: '" . $ts_type_string
+      . "' ts_outfile_string: '" . $ts_outfile_string
+      . "' ts_name_string: '" . $ts_name_string . "'"
+  });
+ 
+  my @maps;
+# my @maps = MT::Template->load({ $ts_name_string, $ts_type_string, $ts_outfile_string, blog_id => $blog->id, });
+  if ($ts_type_string and (! $ts_outfile_string) and (! $ts_name_string)) {
+    mt_log($app, 'Only type selected');
+    @maps = MT::Template->load({ type => "$ts_type_string", blog_id => $blog->id, });
+  } elsif ($ts_outfile_string and (! $ts_name_string)) {
+    mt_log($app, 'Only type and outfile selected');
+    @maps = MT::Template->load({ type => "$ts_type_string", outfile => {like => "$ts_outfile_string" },
+      blog_id => $blog->id, });
+  } elsif ((! $ts_type_string) and $ts_name_string) {
+    mt_log($app, 'Only name selected');
+    @maps = MT::Template->load({ name => {like => "$ts_name_string" }, blog_id => $blog->id, });
+  } elsif ($ts_type_string and (! $ts_outfile_string) and $ts_name_string) {
+    mt_log($app, 'Only type and name selected');
+    @maps = MT::Template->load({ type => "$ts_type_string", name => {like => "$ts_name_string" },
+      blog_id => $blog->id, });
+  } elsif ($ts_outfile_string and $ts_name_string) {
+    mt_log($app, 'Type outfile and name selected');
+    @maps = MT::Template->load({ type => "$ts_type_string", outfile => {like => "$ts_outfile_string" },
+      name => {like => "$ts_name_string" }, blog_id => $blog->id, });
+  } elsif ((! $ts_type_string) and (! $ts_name_string)) {
+    @maps = MT::Template->load();
+  } else {
+    return 1;
+  }
+
+  # extract the template names.
+  my @names = map {$_->name} @maps;
+  # filter out backup templates.
+  my @names = grep {(! /\(Backup \d{4}\-\d{2}\-d{2}/)} @names;
+  my $ts_menu_string = join ('|', @names);
+  
+  $app->log({
+    message => "Saving ts_menu_string '"
+      . $ts_menu_string . "' to database."
+  });
+  
+  $blog->meta('ts_menu_string', $ts_menu_string);
+  $blog->save;
+
+  }   
+
+  return 1;
+}
+
+# HELPER SUBROUTINES
+
+sub mt_log {
+   my ($app, $msg) = @_;
+   $app->log({
+    message => $msg
+  });
+
+}
+
+sub ts_trim_whitespace {
+my $string = shift;
+	$string =~ s/^\s+//;
+	$string =~ s/\s+$//;
+	return $string;
+}
+
+1;
